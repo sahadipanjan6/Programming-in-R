f51d3a3ce9174c9716de0b89ce0a905e17c22234
Dipanjan Saha <sahadipanjan6@gmail.com>
2020-12-09 14:36:54 +0530

    Add files via upload

---
+++ MaximumMinimumElement.R
@@ -0,0 +1,7 @@
+# Write a R program to find the maximum and the minimum value of a given vector.
+
+givenVector <- c(198, 76, 908, 654, 123, 765, 543, 445)
+print("Maxmimum Element: ")
+print(max(givenVector))
+print("Minimum Element: ")
+print(min(givenVector))
