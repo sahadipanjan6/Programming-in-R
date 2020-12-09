e443223268c19180157f1d7bee01b439f8c73ebb
Dipanjan Saha <sahadipanjan6@gmail.com>
2020-12-09 16:36:10 +0530

    Add files via upload

---
+++ SumMeanProductOfVector.R
@@ -0,0 +1,6 @@
+# Write a R program to compute sum, mean and product of a given vector elements.
+
+vec <- c(1, 2, 3, 4, 5)
+print(paste("Sum:", sum(vec)))
+print(paste("Mean:", mean(vec)))
+print(paste("Product:", prod(vec)))
