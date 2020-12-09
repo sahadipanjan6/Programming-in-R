e361b78ea906855f0366e1dcf07cdff9e68acbb1
Dipanjan Saha <sahadipanjan6@gmail.com>
2020-12-09 10:35:39 +0530

    Add files via upload

---
+++ MeanAndSumOfSequence.R
@@ -0,0 +1,9 @@
+# Program2: Write a R program to create a sequence of numbers from 20 to 50
+#           and find the mean of numbers from 20 to 60 and sum of numbers
+#           from 51 to 91.
+
+sequence1 <- (20:50)
+sequence2 <- (20:60)
+sequence3 <- (51:91)
+print(paste("Mean:", mean(sequence2)))
+print(paste("Sum:", sum(sequence3)))
