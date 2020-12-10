0edada1044ba056eff32db3195938f3507ff229f
Dipanjan Saha <sahadipanjan6@gmail.com>
2020-12-10 10:29:59 +0530

    Add files via upload

---
+++ LengthAndDimension.R
@@ -0,0 +1,7 @@
+# Write a R program to create a vector and find the length and the dimension of the vector.
+
+myVector <- seq(1, 20)
+print(paste("Length of Vector:", length(myVector)))
+print("Dimension of Vector: ")
+print(dim(myVector))
+
