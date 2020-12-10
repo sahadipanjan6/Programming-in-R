0cd54cad492a35ce25e023f886cf8363ed919c0d
Dipanjan Saha <sahadipanjan6@gmail.com>
2020-12-10 09:53:55 +0530

    Add files via upload

---
+++ DistinctValues.R
@@ -0,0 +1,6 @@
+# Write a R program to list the distinct values in a vector from a given vector.
+
+myVector1 <- c(1, 1, 2, 5, 4, 2, 8, 9, 4, 10)
+outputVector <- unique(myVector1)
+print("Distinct values are: ")
+print(outputVector)
