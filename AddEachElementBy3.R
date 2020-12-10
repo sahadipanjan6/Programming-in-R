73964b3b73f0e9315baa711c5c35db8656a422e8
Dipanjan Saha <sahadipanjan6@gmail.com>
2020-12-10 11:55:24 +0530

    Add files via upload

---
+++ AddEachElementBy3.R
@@ -0,0 +1,9 @@
+# Write a R program to add 3 to each element in a given vector. 
+# Print the original and new vector.
+
+myVector <- seq(from = 1, to = 20, by = 2)
+print("Original Vector is: ")
+print(myVector)
+outputVector <- myVector + 3
+print("Output Vector is: ")
+print(outputVector)
