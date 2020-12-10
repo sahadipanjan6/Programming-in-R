c615e0f3bf075165a86b0fe8be2c813ac9c5cf5a
Dipanjan Saha <sahadipanjan6@gmail.com>
2020-12-10 11:46:28 +0530

    Add files via upload

---
+++ ValueGreaterThan10.R
@@ -0,0 +1,8 @@
+# Write a R program to test whether the value of the element of a given vector 
+# greater than 10 or not. Return TRUE or FALSE.
+
+myVector <- seq(from = 1, to = 25, by = 4)
+print("Original Vector is: ")
+print(myVector)
+print("Result Generated(> 10): ")
+print(myVector > 10)
